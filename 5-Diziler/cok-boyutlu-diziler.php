53c5a53fad73f897892f7edf6eb28132643a6119
tarikahveci <99441376+tarikahveci@users.noreply.github.com>
2023-02-04 19:12:43 +0300

    Update cok-boyutlu-diziler.php

--- 5-Diziler/cok-boyutlu-diziler.php
+++ 5-Diziler/cok-boyutlu-diziler.php
@@ -10,3 +10,17 @@ $hayvanlar=[
 ];
 print_r($hayvanlar);
 echo $hayvanlar['Etobur Hayvanlar']['Ormanda Yaşayanlar'][0];
+echo '<br>';
+$ucaklar = [
+    'buyuk ucaklar'=>[
+        'savas ucakları'=>[
+            '1.ucak'=>'f16',
+            '2.ucak'=>'f35',
+            '3.ucak'=>'f22'
+        ],
+    ],
+    'kucuk ucaklar'=>[
+        'gezi ucakları','ilaçlama uçakları'
+    ],
+];
+print_r($ucaklar);
